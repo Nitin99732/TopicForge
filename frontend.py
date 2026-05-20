c3fab2e9377115b053ebb2afa46bb9b36f8b24d5
Nitin99732 <bharranitin@gmail.com>
2026-05-20 20:30:23 +0530

    added code for uploading files

--- frontend.py
+++ frontend.py
@@ -1,2 +1,13 @@
 # Libraries
 import streamlit as st
+
+# Upload file
+upload_file = st.file_uploader(label="Upload PDF, TXT or DOCX.", type=["pdf", "txt", "docx"])
+
+
+#File Uploaded Successfully
+if upload_file is not None:
+
+    st.success("File uploaded successfully.")
+
+    st.write("File name: ", upload_file.name)
